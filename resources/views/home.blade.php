b6de0b82195c34ac69edb623a66fefe97966cc41
WatanyouYodthong <watanyou@mutacth.com>
2021-12-01 16:17:15 +0700

    update employee first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Hello, Watanyou Yothong</h1>
+    <div class="container">
+        <a href="{{ route('employee.create') }}"
+            class="btn btn-outline-success">
+            เพิ่มข้อมูลพนักงาน
+        </a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <td>id</td>
+                    <td>name</td>
+                    <td>department</td>
+                    <td>salary</td>
+                    <td>manage</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($employees as $employee)
+                <tr>
+                    <td>{{$employee->id}}</td>
+                    <td>{{$employee->name}}</td>
+                    <td>{{$employee->department}}</td>
+                    <td>{{$employee->salary}}</td>
+                    <td>
+                        <a
+                            href="{{ route('employee.edit',["id" => $employee->id]) }}"
+                            class="btn btn-outline-warning">
+                            แก้ไขข้อมูล
+                        </a>
+                        <form
+                            action="{{ route('employee.destroy',["id" => $employee->id]) }}"
+                            method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button
+                                type="submit"
+                                class="btn btn-outline-danger">
+                                ลบข้อมูล
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
